b1cc2cdab7079f1e4c1b8bdea50c521ae13e554a
Julian Czapski <151388217+akumadev1@users.noreply.github.com>
2024-06-19 18:15:06 +0200

    Update BlogPosts.tsx

--- src/app/components/BlogPosts.tsx
+++ src/app/components/BlogPosts.tsx
@@ -23,6 +23,12 @@ const BlogPosts = () => {
         }
         const data = await response.json();
         setBlogPosts(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An unexpected error occurred');
+        }
       } finally {
         setLoading(false);
       }
